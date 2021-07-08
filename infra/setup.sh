7330a6fe63b828289019e492910796c135e25b0b
Shem Sedrick <ssedrick1@gmail.com>
2021-07-08 15:37:58 -0500

    Fixed broker lookup on setup

--- infra/setup.sh
+++ infra/setup.sh
@@ -2,21 +2,21 @@
 
 docker-compose exec kafka-1 kafka-topics \
   --create \
-  --bootstrap-server localhost:29092 \
+  --bootstrap-server localhost:9092 \
   --replication-factor 2 \
   --partitions 3 \
   --topic pinball.scores
 
 docker-compose exec kafka-1 kafka-topics \
   --create \
-  --bootstrap-server localhost:29092 \
+  --bootstrap-server localhost:9092 \
   --replication-factor 2 \
   --partitions 2 \
   --topic pinball.users
 
 docker-compose exec kafka-1 kafka-topics \
   --create \
-  --bootstrap-server localhost:29092 \
+  --bootstrap-server localhost:9092 \
   --replication-factor 2 \
   --partitions 2 \
   --topic pinball.highscores
