79912f84d1aed348ab463d8141cf6c08c4c848f7
Shem Sedrick <ssedrick1@gmail.com>
2021-07-09 08:49:29 -0500

    Added example solution for Java

--- part1/java/app/src/main/java/kafka/tutorial/java/App.java
+++ part1/java/app/src/main/java/kafka/tutorial/java/App.java
@@ -1,13 +1,16 @@
 package kafka.tutorial.java;
 
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
-import java.time.Duration;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class App {
 
@@ -33,22 +36,46 @@ public class App {
         return properties;
     }
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(getProducerConnectionProperties());
-        var response = producer.send(new ProducerRecord<>("pinball.scores",  "10", "150"));
-        response.get();
+    private static void sendSync(Producer producer, File file) throws Exception {
+        var reader = new BufferedReader(new FileReader(file));
+        var line = reader.readLine();
+        while (line != null) {
+            var parts = line.split(":");
+            assert(parts.length == 2);
+            var response = producer.send(new ProducerRecord<>("pinball.scores",  parts[0], parts[1]));
+            // In this case order matters, so we block waiting for each send to complete.
+            response.get();
 
-        var consumer = new KafkaConsumer<String, String>(getConsumerConnectionProperties());
-        consumer.subscribe(List.of("pinball.scores"));
-
-        var records = consumer.poll(Duration.ofSeconds(5));
-        System.out.println("Found " + records.count() + " records");
-        for (var record : records) {
-            System.out.printf("Key: %s and value: %s was sent to partition %d of topic %s\n",
-                record.key(),
-                record.value(),
-                record.partition(),
-                record.topic());
+            line = reader.readLine();
         }
+    }
+
+    private static void sendAsync(Producer producer, File file) throws Exception {
+        var reader = new BufferedReader(new FileReader(file));
+        var line = reader.readLine();
+        var responses = new ArrayList<Future<RecordMetadata>>();
+        while (line != null) {
+            var parts = line.split(":");
+            assert(parts.length == 2);
+            // In this case, order doesn't matter, so we can send them asynchronously
+            responses.add(producer.send(new ProducerRecord<>("pinball.users", parts[0], parts[1])));
+
+            line = reader.readLine();
+        }
+        // Make sure they all made it
+        for (var response : responses) {
+            response.get();
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        var scoresFile = new File("../../scores.txt");
+        var producer = new KafkaProducer<String, String>(getProducerConnectionProperties());
+
+        sendSync(producer, scoresFile);
+
+        var idsFile = new File("../../ids.txt");
+        sendAsync(producer, idsFile);
     }
 }
