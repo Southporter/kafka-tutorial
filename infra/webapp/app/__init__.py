7a6feb2bb48d075fd5370f4f99f47a2ee2ac5ec1
Shem Sedrick <ssedrick1@gmail.com>
2021-07-12 10:57:36 -0500

    Part2: Example in place. Need to come up with task.

--- infra/webapp/app/__init__.py
+++ infra/webapp/app/__init__.py
@@ -7,20 +7,31 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URI']
 db = SQLAlchemy(app)
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), unique=True, nullable=False)
+
+
 class Score(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(128), unique=False, nullable=True)
+    userId = db.Column(db.Integer, db.ForeignKey(User.id), unique=False, nullable=False, )
     score = db.Column(db.BIGINT)
 
+    user = db.relationship('User', backref=db.backref('user'))
+
     def __repr__(self):
-        return f'{self.username} - {self.score}'
+        return f'{self.user.username} - {self.score}'
 
 
 db.create_all()
 # Initialize DB with some dummy data
 if len(Score.query.all()) == 0:
-    db.session.add(Score(username="wormtail", score="150"))
-    db.session.add(Score(username="snevilus", score="450"))
+    user1 = User(username="beetles")
+    user2 = User(username="van_halen")
+    db.session.add(user1)
+    db.session.add(user2)
+    db.session.add(Score(user=user1, score="150"))
+    db.session.add(Score(user=user2, score="450"))
     db.session.commit()
 
 
